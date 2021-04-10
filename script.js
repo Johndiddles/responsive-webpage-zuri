6e52379b637a6843682b4d1e154bfd9d4186c7f7
JohnDiddles <Johnadepelumi@gmail.com>
2021-04-10 21:31:26 +0100

    finalizing hamburger animation

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 var ham = document.getElementsByClassName('hamburger');
-
+var panel = document.getElementById('links');
 
 function loadPanel() {
     var panel = document.getElementById('links');
-    panel.classList.toggle('nav-links');
-    panel.classList.toggle('panel');
-    console.log(panel.className);
-    console.log(panel);
-    console.log(panel.classList)
+    var menu = document.getElementById('hamburger');
+
+    if (panel.className === 'nav-links') {
+        panel.classList.replace('nav-links', 'panel');
+    } else {
+        panel.classList.replace('panel', 'nav-links');
+        panel.style.transition = 'max-height 1s'
+    }
+    
+    if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+        panel.style.transition = 'max-height 1s';
+        menu.style.transform = 'rotate(90deg)';  
+    } else {
+        panel.style.maxHeight = '250px';
+        panel.style.transition = 'max-height 1s';
+        panel.style.background = 'white';
+        panel.style.color = '#304353';
+        menu.style.transform = "rotate(180deg)";
+    }
 }
+
